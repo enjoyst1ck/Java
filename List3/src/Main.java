6da6e335fbe35c89740ce1559867d017dc078f2c
Kamil <134055@student.uni.opole.pl>
2023-04-16 14:28:11 +0200

    Dodano format daty i pobranie od użytkownika daty

--- List3/src/Main.java
+++ List3/src/Main.java
@@ -1,7 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
 
-        System.out.println("Hello worlds!");
-        System.out.println("Test");
+public class Main {
+
+    public static void zad4() throws ParseException{
+        Scanner scan = new Scanner(System.in);
+        String pattern = "MM.dd.yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        System.out.println("Podaj date w formacie MM.dd.yyyy");
+        String data1 = scan.nextLine();
+        Date date1 = dateFormat.parse(data1);
+
+        System.out.println("Podaj date w formacie MM.dd.yyyy");
+        String data2 = scan.nextLine();
+        Date date2 = dateFormat.parse(data2);
+
+    }
+    public static void main(String[] args) throws ParseException{
+        zad4();
     }
 }
