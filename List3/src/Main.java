1efc6048471703b2519da1d6dac0ef28b8392dfa
enjoyst1ck <bwwojcik0@gmail.com>
2023-04-16 14:32:40 +0200

    Dodano strefy czasowe i ustawiono date

--- List3/src/Main.java
+++ List3/src/Main.java
@@ -16,6 +16,22 @@ public class Main {
         System.out.println("Podaj date w formacie MM.dd.yyyy");
         String data2 = scan.nextLine();
         Date date2 = dateFormat.parse(data2);
+    }
+
+    public static void zad5() {
+        TimeZone timeZone1 = TimeZone.getTimeZone("America/Los_Angeles");
+        TimeZone timeZone2 = TimeZone.getTimeZone("Europe/Warsaw");
+
+        Calendar calendar1 = Calendar.getInstance(timeZone1);
+        calendar1.set(2023, 6, 5, 1,30,0);
+
+
+
+
+        calendar1.setTimeZone(timeZone2);
+
+
+
 
     }
     public static void main(String[] args) throws ParseException{
