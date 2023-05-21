1e2d97fe197e02c9210986ea7b220154832a6ac3
enjoyst1ck <bwwojcik0@gmail.com>
2023-05-21 20:25:26 +0200

    Uzupełnienie zadania 1

--- List8/Zad1/src/Main.java
+++ List8/Zad1/src/Main.java
@@ -3,9 +3,25 @@ public class Main {
         //Index spoza zakresu danej tablicy
         try {
             int[] tab = {1, 2, 3};
+
+            int index1 = tab[2];
+            System.out.println(index1);
+
             int x = tab[4];
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Index spoza tablicy.");
+        }
+
+        //ArithmeticException  - gdy dzielimy przez zero
+        try {
+            int a = 6 / 2;
+            System.out.println(a);
+
+            int z = 5 / 0;
+            System.out.println(z);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Dzielenie przez zero.");
         }
 
         //NullPointerException - gdzy odwolujemy sie do obiektu który ma wartosc null
