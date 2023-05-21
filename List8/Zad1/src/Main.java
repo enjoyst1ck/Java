c3abec2c49d6a4f58c6bfeea90f04e5162876ec0
Kamil <134055@student.uni.opole.pl>
2023-05-21 20:21:51 +0200

    Dodanie części zadania pierwszego

--- List8/Zad1/src/Main.java
+++ List8/Zad1/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Index spoza zakresu danej tablicy
+        try {
+            int[] tab = {1, 2, 3};
+            int x = tab[4];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index spoza tablicy.");
+        }
+
+        //NullPointerException - gdzy odwolujemy sie do obiektu który ma wartosc null
+        try {
+            String text = null;
+            int textLength = text.length();
+        } catch (NullPointerException e){
+            System.out.println("Obiekt jest pusty.");
+        }
     }
 }
